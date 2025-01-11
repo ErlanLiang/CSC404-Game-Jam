cfae128fc83fe420d9992b58223356eeaba37492
ShakazZ <erlan20011012@gmail.com>
2025-01-11 13:37:21 -0500

    Small changes

--- Assets/Scripts/initStatus.cs
+++ Assets/Scripts/initStatus.cs
@@ -5,7 +5,8 @@ public class initStatus : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //initialize the sliders to 50
+
     }
 
     // Update is called once per frame
