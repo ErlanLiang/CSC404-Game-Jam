9f0f9a4f70cdc313564c71176666e833ca3bf04b
ShakazZ <erlan20011012@gmail.com>
2025-01-11 11:43:10 -0500

    Initialized player

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class player : MonoBehaviour
+{   
+    public float budyHeat = 50.0f;
+    public float stamina = 50.0f;
+    public float embarrasment = 50.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
